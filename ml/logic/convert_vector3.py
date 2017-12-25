affcd48301527565ac405e7b8148c0e0add3063d
shimakaze-git <shimakaze.soft+github@googlemail.com>
2017-12-25 14:30:15 +0000

    machine-learnning辺りの処理をコミット

---
+++ ml/logic/convert_vector3.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*- 
+import os
+from PIL import Image
+
+class TestClass:
+
+    def __init__(self, code, name):
+        self.code = code
+        self.name = name
+    
+    def getDatas(self,id):
+        """
+        
+        """
+        
+        ##
+        # DBから女優名を引き出し、画像にアクセスして画像を引き出す
+        ##
+        
+        # files = ["faces/" + f for f in os ]
+        labels = []
+        datas = []
+        
+        for image in files:
+            as_arrayed_img = np.asarray()
+        
+        
+        
+        print('test')
+        return None
--- ore_konomi_joshi/settings.py
+++ ore_konomi_joshi/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'user',
+    'ml',
 ]
 
 MIDDLEWARE_CLASSES = [
